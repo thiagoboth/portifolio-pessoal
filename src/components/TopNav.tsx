1fc8184c201390cf05d0b85c7051cceda6bbb8c3
thiagobotelho <thiagoboth@hotmail.com>
2025-08-21 00:59:51 -0300

    Add MenuIcon component, update TopNav layout, and configure static export settings

--- src/components/TopNav.tsx
+++ src/components/TopNav.tsx
@@ -1,20 +1,28 @@
 "use client";
 import Image from "next/image";
+import {MenuIcon} from "@/components/icons/menu-icon";
 
 export default function TopNav() {
     return (
-        <nav className="sticky top-0 z-50 border-b border-gray/20 bg-white/80 flex items-center p-2">
-            <Image
-                src="/logo.svg"
-               alt="Logo"
-               width={13}
-               height={23}
-               className="w-4 h-6 object-contain"
-            />
+        <nav className="sticky top-0 z-50 border-b border-gray/20 bg-white/80 flex justify-between items-center p-2">
+            <div className={"flex items-center"}>
+                <Image
+                    src="/logo.svg"
+                    alt="Logo"
+                    width={13}
+                    height={23}
+                    className="w-8 h-10 object-contain"
+                />
 
-            <div className={"ml-1"}>
-                <p className="font-gothic [font-size:8px]">Thiago Botelho</p>
-                <p className="font-calibri [font-size:10px]">DESENVOLVEDOR, UX/UI DESIGNER</p>
+                <div className={"ml-1"}>
+                    <p className="font-gothic text-lg">Thiago Botelho</p>
+                    <p className="font-calibri text-xs">DESENVOLVEDOR, UX/UI DESIGNER</p>
+                </div>
+            </div>
+
+            <div className={"flex items-center gap-1 bg-red cursor-pointer group"}>
+                <p className="font-calibri text-lg text-gray group-active:text-gray-light transition-colors">Menu</p>
+                <MenuIcon />
             </div>
         </nav>
     );
---
+++ src/components/icons/menu-icon.tsx
@@ -0,0 +1,19 @@
+export function MenuIcon() {
+    return (
+        <svg
+            className="w-8 h-8 stroke-gray group-active:stroke-gray-light transition-colors"
+            fill="none"
+            viewBox="0 0 24 24"
+            strokeWidth={1.5}
+            stroke="currentColor"
+            aria-hidden="true"
+            xmlns="http://www.w3.org/2000/svg"
+        >
+            <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="M3.75 9h16.5m-16.5 6.75h16.5"
+            />
+        </svg>
+    );
+}
